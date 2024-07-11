925848db3ef8b5b66bf0bda1d0ddcccd20ff219c
zhangzhao219 <zhaozhao809@163.com>
2024-07-11 17:13:21 +0800

    update

--- Interview/sqrt.cpp
+++ Interview/sqrt.cpp
@@ -19,11 +19,12 @@ double gdtosqrt(double x, double threshold){
     // y^2 = x -> x - y^2 = 0
     // min (x - y^2)^2
     // x^2 - 2 * x * y^2 + y^4
+    // 对y求导
     // -4*x * y + 4 * y ^ 3 
 
     double now = x;
-    double pre = x + threshold + 1;
-    double lr = 0.0001;
+    double pre = x + threshold*2;
+    double lr = 0.00001;
     while(abs(now-pre) >= threshold){
         pre = now;
         now = now - lr * (-4 * x * now + 4 * now * now * now);
@@ -31,11 +32,32 @@ double gdtosqrt(double x, double threshold){
     return now;
 }
 
+double gdtocube(double x, double threshold){
+    // y^3 = x -> x - y^3 = 0
+    // min (x - y^3)^2
+    // x^2 - 2 * x * y^3 + y^6
+    // 对y求导
+    // -6*x * y^2 + 6 * y ^ 5
+
+    double now = x;
+    double pre = x + threshold*2;
+    double lr = 0.00001;
+    while(abs(now-pre) >= threshold){
+        pre = now;
+        now = now - lr * (-6 * x * now * now + 6 * now * now * now * now * now);
+    }
+    
+    return now;
+}
+
 int main(){
-    double a = 0.1;
-    double res = binarysearch(a, 0.0000001);
+    double a = 1;
+    double threshold = 0.00000000001;
+    double res = binarysearch(a, threshold);
     cout << res << endl;
-    double res2 = gdtosqrt(a, 0.0000001);
+    double res2 = gdtosqrt(a, threshold);
     cout << res2 << endl;
+    double res3 = gdtocube(a, threshold);
+    cout << res3 << endl;
     return 0;
 }
